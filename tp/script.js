eb207b43c78a55bfe7792f95a0df08afe4914ff0
Carlos <cgobbo@uade.edu.ar>
2025-06-09 21:14:35 -0300

    Delete tp directory

--- tp/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggleButton = document.getElementById('toggle-darkmode');
-  toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    toggleButton.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
-  });
-});
